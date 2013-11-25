6cdf5805315b996ffaf9faa7f031c98250e3eea6
Hamid Mohseni <hamid2262@yahoo.com>
2013-11-25 16:50:51 +0100

    homes controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Shouter::Application.routes.draw do
+  
+  root 'homes#show'
+  resources :homes, only: ['show']
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
