09b8268ee0cadd02a4a3ead52f6bf7889265c74c
Hamid Mohseni <hamid2262@yahoo.com>
2013-12-12 00:02:51 +0100

    modify trip edit page with it's subtrips

--- app/models/subtrip.rb
+++ app/models/subtrip.rb
@@ -2,4 +2,6 @@ class Subtrip < ActiveRecord::Base
   belongs_to :trip
   belongs_to :origin, class_name: "City", foreign_key: "origin_id"
   belongs_to :destination, class_name: "City", foreign_key: "destination_id"
+
+  default_scope { order('datetime ASC') } 
 end
---
+++ config/initializers/dates_format.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:date] = "%A, %d %B"
+Time::DATE_FORMATS[:time] = "%H:%M"
