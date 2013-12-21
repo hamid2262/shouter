26dd724f41f427644e0e73f2c6df092c18162681
Hamid Mohseni <hamid2262@yahoo.com>
2013-12-21 05:19:38 +0100

    find subtips by given search data in subtrip_search.rb

--- app/models/search_subtrip.rb
+++ app/models/search_subtrip.rb
@@ -12,17 +12,29 @@ class SearchSubtrip
 		@destination_lng = options.fetch(:destination_lng, "")
 		@destination_cycle = options.fetch(:destination_cycle, "")
 		@date = convert_jalali_to_gregorian( options.fetch(:date, "") )
-
 	end
 
 	def subtrips days=0
+		origin_ids = cities_near_origin.map { |d| d.id }
+		destination_ids = cities_near_destination.map { |d| d.id }
+
 		# start_date = DateTime.parse(date) 
 		# end_date = start_date.end_of_day + days.days
-	  # subtrips = Subtrip.where(origin_id: origin_id, destination_id: destination_id)
+	  subtrips = Subtrip.where("origin_id IN (?)", origin_ids ) 
+	  subtrips.where("destination_id IN (?)", destination_ids ) 
+	  	# , destination_id: destination_id)
 		# subtrips.where("datetime > ?", start_date).where("datetime < ?", end_date)		
 	end
 
 	private
+		def cities_near_origin
+			City.near([origin_lat,origin_lng],origin_cycle.to_f/1.609344 )
+		end
+
+		def cities_near_destination
+			City.near([destination_lat,destination_lng],destination_cycle.to_f/1.609344 )
+		end
+
 		def convert_jalali_to_gregorian date
 			date = date.split("/").reverse
 			if (date[0].to_i > 1391 && date[0].to_i <= 1400) && (date[1].to_i > 0 && date[1].to_i <= 12) && (date[2].to_i > 0  && date[2].to_i < 32)
