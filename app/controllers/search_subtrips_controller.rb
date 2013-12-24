fa60a56af9db9aa65f5da675bc545f0e5106ba9c
Hamid Mohseni <hamid2262@yahoo.com>
2013-12-24 15:46:37 +0100

    sending object of SearchSubtrip to view

--- app/controllers/search_subtrips_controller.rb
+++ app/controllers/search_subtrips_controller.rb
@@ -3,9 +3,10 @@ class SearchSubtripsController < ApplicationController
 	skip_authorization_check	
 
 	def show
+		@subtrips = SearchSubtrip.all
 	end
 
 	def search
-		@search_subtrips = SearchSubtrip.new(params[:search_subtrip])			
+		@search_subtrip = SearchSubtrip.new(params[:search_subtrip])			
 	end
 end
--- app/models/search_subtrip.rb
+++ app/models/search_subtrip.rb
@@ -1,19 +1,35 @@
 class SearchSubtrip 
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+  
+	attr_accessor :origin_id, :origin_lat, :origin_lng, :origin_cycle, 
+								:destination_id, :destination_lat, :destination_lng, :destination_cycle, 
+								:date
 
-	include ActiveModel::Conversion
-	attr_reader :origin_lat, :origin_lng, :origin_cycle, :destination_lat, 
-							:destination_lng, :destination_cycle, :date
-
-	def initialize options ={}
-		@origin_lat = options.fetch(:origin_lat, "")
-		@origin_lng = options.fetch(:origin_lng, "")
-		@origin_cycle = options.fetch(:origin_cycle, "")
-		@destination_lat = options.fetch(:destination_lat, "")
-		@destination_lng = options.fetch(:destination_lng, "")
-		@destination_cycle = options.fetch(:destination_cycle, "")
-		@date = convert_jalali_to_gregorian( options.fetch(:date, "") )
+	def initialize attributes ={}
+    unless attributes.nil?
+      attributes.each do |name, value|
+        send("#{name}=", value)
+      end
+  		@date = convert_jalali_to_gregorian( attributes.fetch(:date, "") )
+    end
+		# @origin_lat = attributes.fetch(:origin_lat, "")
+		# @origin_lng = attributes.fetch(:origin_lng, "")
+		# @origin_cycle = attributes.fetch(:origin_cycle, "")
+		# @destination_lat = attributes.fetch(:destination_lat, "")
+		# @destination_lng = attributes.fetch(:destination_lng, "")
+		# @destination_cycle = attributes.fetch(:destination_cycle, "")
+		# @date = convert_jalali_to_gregorian( attributes.fetch(:date, "") )
 	end
 
+  def persisted?
+    false
+  end
+
+  def self.all
+    Subtrip.all
+  end
 	def subtrips days=0
 		origin_ids = cities_near_origin.map { |d| d.id }
 		destination_ids = cities_near_destination.map { |d| d.id }
@@ -22,8 +38,10 @@ class SearchSubtrip
 		# end_date = start_date.end_of_day + days.days
 	  subtrips = Subtrip.where("origin_id IN (?)", origin_ids ) 
 	  subtrips.where("destination_id IN (?)", destination_ids ) 
+
 	  	# , destination_id: destination_id)
 		# subtrips.where("date_time > ?", start_date).where("date_time < ?", end_date)		
+		# subtrips = Subtrip.all
 	end
 
 	private
@@ -37,7 +55,9 @@ class SearchSubtrip
 
 		def convert_jalali_to_gregorian date
 			date = date.split("/").reverse
-			if (date[0].to_i > 1391 && date[0].to_i <= 1400) && (date[1].to_i > 0 && date[1].to_i <= 12) && (date[2].to_i > 0  && date[2].to_i < 32)
+			if (date[0].to_i > 1391 && date[0].to_i <= 1400) && 
+				 (date[1].to_i > 0 && date[1].to_i <= 12) && 
+				 (date[2].to_i > 0  && date[2].to_i < 32)
 				date_obj = JalaliDate.new( date[0].to_i, date[1].to_i, date[2].to_i )
 			else
 				date_obj = JalaliDate.new(Date.today)
