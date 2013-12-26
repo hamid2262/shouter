8df5d09d118ccd2b060f37dee9b68e6f0fe1ec78
Hamid Mohseni <hamid2262@yahoo.com>
2013-12-26 21:40:52 +0100

    improve insert trip form with bootstrap and scss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,7 +52,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    link_to(name, '#', class: "add_fields btn btn-primary btn-sm", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields btn btn-default btn-sm", data: {id: id, fields: fields.gsub("\n", "")})
   end	
 
 	def flash_creator flash
