4beae18213e929690b33639db0c37f92c7c4723d
Hamid Mohseni <hamid2262@yahoo.com>
2014-01-08 03:20:09 +0100

    improve dashboard friend list that unfolllowed list should not include followed users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,8 +4,8 @@ class DashboardsController < ApplicationController
 	skip_authorization_check
 	def show
 		@dashboard = Dashboard.new(current_user)
-		@followed_users = current_user.followed_users.order(updated_at: :desc)
-		@random_users = User.where.not(id: current_user.id).order(updated_at: :desc)
+		@followed_users = @dashboard.followed_users
+		@random_users = @dashboard.random_users
 	end
 
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -11,14 +11,26 @@ class Dashboard
 	def new_photo_shout
 		PhotoShout.new
 	end
-
-	def timeline 
+	
+	def timeline
 		@shouts = Shout.where(user_id: shout_user_ids)
 	end
 
-	private
-	
-	def shout_user_ids
-		@user.followed_users.map{|user| user.id} + [@user.id]		
+	def followed_users
+		@user.followed_users.order(updated_at: :desc)
 	end
+
+	def random_users
+		User.where.not(id: unfollowed_users_ids).order(updated_at: :desc)
+	end
+
+	private
+
+		 def unfollowed_users_ids
+			ids = @user.followed_users.map{|user| user.id} + [@user.id]
+		 end
+
+		def shout_user_ids
+			@user.followed_users.map{|user| user.id} + [@user.id]
+		end
 end
