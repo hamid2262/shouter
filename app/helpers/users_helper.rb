65332a9747d1224082f88a44cfa73df7e90f0ffb
Hamid Mohseni <hamid2262@yahoo.com>
2014-01-17 06:22:40 +0100

    a helper method for static form controls, used in user edit form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,5 +8,19 @@ module UsersHelper
 		end
 	end
 
+	def user_form_control_static title, field_value
+		field_value = 'No Data' if field_value.blank?
+		html = <<-HTML
+		  <div class="form-group">
+		    <label class="col-sm-3 control-label">#{title}</label>
+		    <div class="col-sm-9">
+		      <p class="form-control-static">
+						#{field_value}
+		      </p>
+		    </div>
+		  </div>
+		HTML
+		html.html_safe 
+	end
 end
 
