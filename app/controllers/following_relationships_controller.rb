66fb0eece00530d803bb1019fb7c84d74ae16438
Hamid Mohseni <hamid2262@yahoo.com>
2014-01-17 05:53:49 +0100

    in change user url to profile_url

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -2,16 +2,15 @@ class FollowingRelationshipsController < ApplicationController
 
   load_and_authorize_resource 
   skip_load_resource only: [:create] 
-
 	
 	def create
 		current_user.follow user
-		redirect_to user, notice: "Now you are following #{user.name}."
+		redirect_to profile_url(user), notice: "Now you are following #{user.name}."
 	end
 
 	def destroy
 		current_user.unfollow user
-		redirect_to user, notice: "#{user.name} unfollowed."
+		redirect_to profile_url(user), notice: "#{user.name} unfollowed."
 	end
 
 	private
