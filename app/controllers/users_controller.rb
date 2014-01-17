54a489e59719d99d16e09045ed2e386338d808ab
Hamid Mohseni <hamid2262@yahoo.com>
2014-01-17 06:11:34 +0100

    change users show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < Devise::RegistrationsController
 
-  layout 'application_user', only: [:show]
+  # layout 'application_user', only: [:show]
   
   load_and_authorize_resource only: [:index, :show, :update, :edit]
 
@@ -12,10 +12,7 @@ class UsersController < Devise::RegistrationsController
 
   def show
     @user = User.find(params[:id])
-    @shouts = @user.shouts
     @vehicle = @user.vehicle
-    @followed_users = @user.followed_users.order(updated_at: :desc)
-    # @shouts = Shout.where(:user_id, params[:id])
   end
   
   def edit
@@ -23,28 +20,37 @@ class UsersController < Devise::RegistrationsController
   end
 
   def update
-    @user = User.find(current_user.id)
-
     # https://github.com/plataformatec/devise/wiki/How-To%3a-Allow-users-to-edit-their-account-without-providing-a-password
-    successfully_updated = if needs_password?(@user, user_params)
-      @user.update_with_password(user_params)
+    successfully_updated = if needs_password?(current_user, user_params)
+      current_user.update_with_password(user_params)
     else
       params[:user].delete(:current_password)
       if current_user.is_admin?
-        @user.update_without_password(admin_user_params)          
+        current_user.update_without_password(admin_user_params)          
       else
-        @user.update_without_password(user_without_password_params)  
+        current_user.update_without_password(user_without_password_params)  
       end
     end
 
     if successfully_updated
-      redirect_to @user , notice: "Profile was successfully updated."
+      if check_for_cover_or_avatar_update?
+        redirect_to profile_url(current_user) , notice: "Image was successfully updated."
+      else
+        redirect_to user_path(current_user)+'#'+params[:user][:tab] , notice: "Profile was successfully updated."        
+      end
     else
       render action: 'edit', locals: {tab: params[:user][:tab]}
     end
   end
 
 private
+
+  def check_for_cover_or_avatar_update?
+    false
+    cover_updated  = (Time.now - current_user.cover_updated_at < 10) if current_user.cover_updated_at
+    avatar_updated = (Time.now - current_user.avatar_updated_at < 10) if current_user.avatar_updated_at
+    true if cover_updated || avatar_updated
+  end
 
   def require_login
     unless user_signed_in?
