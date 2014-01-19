71c60e08d6550e28c93b43fd0c10d7ddd959507e
Hamid Mohseni <hamid2262@yahoo.com>
2014-01-19 04:03:28 +0100

    i18n for profile show

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,3 +1,11 @@
 module ProfilesHelper
-
+	def force_to_sign_in_message
+		html = <<-HTML
+			<h5 class="text-center"> 
+				#{t('profiles.show.force_login_message')}
+				 #{link_to(t('sign_in'), new_user_session_path )}
+			</h5>
+		HTML
+		html.html_safe
+	end
 end
