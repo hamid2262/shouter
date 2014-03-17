32680eda79938ada38f68e2c79372ac160ab9867
Hamid Mohseni <hamid2262@yahoo.com>
2014-03-17 01:38:32 +0100

    change style of peofile sidebar for user details

--- app/models/network.rb
+++ app/models/network.rb
@@ -20,4 +20,21 @@ class Network
 		user
 	end
 
+	def self.network_size user
+		count = 0
+		user.invited_users.each do |child1|
+			count += 1
+			child1.invited_users.each do |child2|
+				count += 1
+				child2.invited_users.each do |child3|
+					count += 1
+					child3.invited_users.each do |child4|
+						count += 1
+					end
+				end
+			end
+		end
+		count
+	end
+
 end
