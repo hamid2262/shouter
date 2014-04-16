2a3281ae174285b755f9451157e5db9f3ac5ec65
Hamid Mohseni <hamid2262@yahoo.com>
2014-04-16 16:17:03 +0200

    fix bug: in shouts hide destination as a subtrip

--- app/models/home.rb
+++ app/models/home.rb
@@ -3,6 +3,7 @@ class Home
 	def self.newtrips
     subtrips = Subtrip.where.not("origin_address = destination_address").where("active = true")
     subtrips = subtrips.where("date_time > ?", DateTime.now)
+    subtrips = subtrips.where("created_at > ?", DateTime.now - 4.days)
     subtrips = subtrips.order(date_time: :desc)		
 	end
 
