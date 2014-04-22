447f361f8a2d0b9303812ef81a54808d4c8340eb
Hamid Mohseni <hamid2262@yahoo.com>
2014-04-22 16:56:14 +0200

    improve performance of home page by eger loading

--- app/models/home.rb
+++ app/models/home.rb
@@ -3,6 +3,8 @@ class Home
 	def self.newtrips
     subtrips = Subtrip.where.not("origin_address = destination_address").where("active = true")
     subtrips = subtrips.where("date_time > ?", DateTime.now)
+    subtrips = subtrips.includes(:trip => :driver)
+    subtrips = subtrips.includes(:trip => :currency)
     subtrips = subtrips.order(date_time: :desc)		
 	end
 
